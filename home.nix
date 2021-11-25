80489ea2ac0b53a4b9b53939662738a4f20f6b41
Jeferson David Ossa <jedossa@gmail.com>
2021-11-25 18:13:43 +0100

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ zsh-completions thefuck autojump sbt ammonite jetbrains.idea-community google-chrome vscode ];
+  home.packages = with pkgs; [ zsh-completions thefuck autojump sbt coursier ammonite jetbrains.idea-community google-chrome vscode rustup ];
 
   programs.home-manager.enable = true;
 
-  programs.git = { 
+  programs.git = {
     enable = true;
     userName = "jedossa";
     userEmail = "jedossa@gmail.com";
